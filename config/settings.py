41cb1664e24b6a188a869cd9f96d8ef4c76391e5
nyagacollins <ndegecollins20@gmail.com>
2025-10-31 14:54:17 +0300

    final commit

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from decouple import config
+import os
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
